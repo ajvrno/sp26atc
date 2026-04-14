c57003d18b01d42b12e9b7b9a241c1758e4c5787
Ashley Rabino <ashleyjanelle.rabino@yahoo.com>
2026-04-14 15:13:30 -0400

    Stop tracking files listed in .gitignore

--- db_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = 'localhost';
-$username = 'root';
-$password = 'Qbyz@143';
-$dbname = 'asc_dropin_tutoring';
-
-// Create the connection
-$db = mysqli_connect($host, $username, $password, $dbname);
-?>
