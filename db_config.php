2dc443c3145ee0ded6c1f960b0d74380314514b1
Ashley Rabino <ashleyjanelle.rabino@yahoo.com>
2026-04-14 11:19:57 -0400

    trying to merge adminhtml to main

---
+++ db_config.php
@@ -0,0 +1,9 @@
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = 'Qbyz@143';
+$dbname = 'asc_dropin_tutoring';
+
+// Create the connection
+$db = mysqli_connect($host, $username, $password, $dbname);
+?>
